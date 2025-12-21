6f45b0e6734e4ea0c95b14f9f70c81b9c52c680d
esaiemp <126763768+esaiemp@users.noreply.github.com>
2025-12-21 03:20:02 -0500

    Updates

--- _next/static/9pM9F9oJL3skzVpSghvg5/_buildManifest.js
+++ _next/static/9pM9F9oJL3skzVpSghvg5/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/datafixer//_next/:path+",
+        "destination": "/datafixer/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
